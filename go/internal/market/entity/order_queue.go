54ec5cb9f5efcac5d54238b6b457a41a3e9ba991
Nicholas Cabral <nicholasscabral@gmail.com>
2023-06-26 12:17:43 -0300

    chore: rename files

--- go/internal/market/entity/order_queue.go
+++ go/internal/market/entity/order_queue.go
@@ -5,7 +5,7 @@ type OrderQueue struct {
 }
 
 func (oq *OrderQueue) Less(i, j int) bool {
-	return oq.Orders[i].price < oq.Orders[j].price
+	return oq.Orders[i].Price < oq.Orders[j].Price
 }
 
 func (oq *OrderQueue) Swap(i, j int) {
@@ -16,11 +16,11 @@ func (oq *OrderQueue) Len() int {
 	return len(oq.Orders)
 }
 
-func (oq *OrderQueue) Push(x any) {
+func (oq *OrderQueue) Push(x interface{}) {
 	oq.Orders = append(oq.Orders, x.(*Order))
 }
 
-func (oq OrderQueue) Pop() any {
+func (oq *OrderQueue) Pop() interface{} {
 	old := oq.Orders
 	n := len(old)
 	item := old[n-1]
